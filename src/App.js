8b4162a561ec8311e1c44e57040f2506b8fd67e5
Kevin Griego <kev.gri32@gmail.com>
2018-11-20 12:21:04 -0700

    duplicate dupy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Amplify, { Auth } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react";
+import aws_exports from "./aws-exports";
+
+import * as chirps from "./dummy_data/chirps";
+import Header from "./Components/Header";
+import Login from "./Components/Login";
 import Feed from "./Views/Feed";
-import Amplify, { Auth } from 'aws-amplify';
-import { withAuthenticator } from 'aws-amplify-react';
-import aws_exports from './aws-exports';
 
-import * as chirps from './dummy_data/chirps';
-import Header from './Components/Header';
-import Login from './Components/Login';
-import Feed from './Views/Feed';
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 Amplify.configure(aws_exports);
 library.add(faPlus);
 
