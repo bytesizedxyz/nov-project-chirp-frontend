36c61acb342d29757687d4142dd97c8695a31ae7
David Kruse <thedavidkruse@gmail.com>
2018-11-20 13:01:25 -0700

    added icons for post

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -2,16 +2,18 @@
 // {"userId":7,"message":"Distributed solution-oriented contingency","deleted":true,"likes":16,"dislikes":33,"favorites":73,"created_at":"8/25/2001"},
 import React from 'react';
 import "./Post.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp, faThumbsDown, faStar, faClock } from '@fortawesome/free-solid-svg-icons';
 
 export default function Post(props) {
   const chirp = props.chirp
   return (
     <div className="post">
       <span className="content">{chirp.message}</span>
-      <span className="likes">{chirp.likes} Likes</span>
-      <span className="dislikes">{chirp.dislikes} Dislikes</span>
-      <span className="favorites">{chirp.favorites} Favorites</span>
-      <span className="createdAt">{chirp.created_at}</span>
+      <span className="likes">{chirp.likes} <FontAwesomeIcon icon={faThumbsUp}/></span>
+      <span className="dislikes">{chirp.dislikes} <FontAwesomeIcon icon={faThumbsDown}/></span>
+      <span className="favorites">{chirp.favorites} <FontAwesomeIcon icon={faStar}/></span>
+      <span className="createdAt">{chirp.created_at} <FontAwesomeIcon icon={faClock}/></span>
     </div>
   )
 }
