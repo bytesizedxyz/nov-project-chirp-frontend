4b0484490c191ea7b6df29d2e554d522a0858f6e
Christopher White <christopher.evan.white88@gmail.com>
2018-11-20 11:44:23 -0700

    merged conflict

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-import Amplify, { Auth } from "aws-amplify";
-import { withAuthenticator } from "aws-amplify-react";
-import aws_exports from "./aws-exports";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Amplify, { Auth } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import aws_exports from './aws-exports';
 
-import * as chirps from "./dummy_data/chirps";
-import Header from "./Components/Header";
-import Login from "./Components/Login";
-import Feed from "./Views/Feed";
+import * as chirps from './dummy_data/chirps';
+import Header from './Components/Header';
+import Login from './Components/Login';
+import Feed from './Views/Feed';
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 Amplify.configure(aws_exports);
 library.add(faPlus);
 
@@ -27,12 +27,6 @@ class App extends Component {
   render() {
     const { chirps } = this.state;
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <header className="App-header" />
-        <Feed chirps={chirps} />
-      </div>
-=======
       <Router>
         <div className="App">
           <Header />
@@ -42,7 +36,6 @@ class App extends Component {
           <Feed chirps={chirps} />
         </div>
       </Router>
->>>>>>> 0fad51276734916007541a2208a894536d41a273
     );
   }
 }
