b524c350aab370c8ba371fda2208fe70ffb176da
Kevin Griego <kev.gri32@gmail.com>
2018-11-21 14:04:45 -0700

    context provider switching button theme muahahhahahahahahahahhahahaha

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,6 +3,7 @@ import Gravatar from "gravatar-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import EmpireLogo from "./empire-brands";
 import Modal from "./Modal";
+import { ThemeContext } from "../ThemeProvider";
 import "./header.css";
 
 class Header extends Component {
@@ -48,52 +49,65 @@ class Header extends Component {
         />
       );
     }
+
     return (
-      <div className="flexColumnAround">
-        <div className="flexRowBetween">
-          <EmpireLogo />
-          <h1>Darth Twitter</h1>
-          {GravatarBlock}
-        </div>
+      <ThemeContext.Consumer>
+        {({ theme, toggleTheme }) => (
+          <div className="flexColumnAround">
+            <div className="flexRowBetween">
+              <EmpireLogo />
+              <h1>Darth Twitter</h1>
+              {GravatarBlock}
+            </div>
 
-        <div className="headerBottom flexRowAround">
-          <Modal
-            open={this.state.show}
-            handleClose={this.hideModal}
-            addPost={this.closeAndSend}
-          >
-            <h1>Add New Post</h1>
-            <textarea
-              value={this.state.message}
-              onChange={this.handleChange}
-              rows="5"
-              cols="50"
-              type="text"
-              name="message"
-              maxLength="280"
-            />
-          </Modal>
+            <div className="headerBottom flexRowAround">
+              <Modal
+                open={this.state.show}
+                handleClose={this.hideModal}
+                addPost={this.closeAndSend}
+              >
+                <h1>Add New Post</h1>
+                <textarea
+                  value={this.state.message}
+                  onChange={this.handleChange}
+                  rows="5"
+                  cols="50"
+                  type="text"
+                  name="message"
+                  maxLength="280"
+                />
+              </Modal>
 
-          <button className="chirpButton" onClick={this.showModal}>
-            <FontAwesomeIcon icon="plus" />
-          </button>
+              <button className="chirpButton" onClick={this.showModal}>
+                <FontAwesomeIcon icon="plus" />
+              </button>
 
-          <span className="flexRowCenter">
-            <FontAwesomeIcon icon="search" className="searchIcon" />
-            <input
-              onChange={handleFilter}
-              placeholder="Search for chirps"
-              className="search"
-              type="text"
-              name={filter}
-            />
-          </span>
+              <button
+                className="chirpButton"
+                onClick={toggleTheme}
+                style={{ backgroundColor: theme.background }}
+              >
+                Change Theme
+              </button>
 
-          <button className="chirpButton" onClick={this.handleSearch}>
-            SEARCH
-          </button>
-        </div>
-      </div>
+              <span className="flexRowCenter">
+                <FontAwesomeIcon icon="search" className="searchIcon" />
+                <input
+                  onChange={handleFilter}
+                  placeholder="Search for chirps"
+                  className="search"
+                  type="text"
+                  name={filter}
+                />
+              </span>
+
+              <button className="chirpButton" onClick={this.handleSearch}>
+                SEARCH
+              </button>
+            </div>
+          </div>
+        )}
+      </ThemeContext.Consumer>
     );
   }
 }
