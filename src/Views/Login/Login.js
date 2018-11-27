5e92cc2a7c61e58e6c765b712a715345f5b66a90
Kevin Griego <kev.gri32@gmail.com>
2018-11-27 16:00:15 -0700

    moved feed test into feed folder

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -1,68 +1,60 @@
-import React, { Component } from 'react';
-import './Login.css';
+import React, { Component } from "react";
+import "./Login.css";
 
 import AuthService from "../../Services/AuthService";
 
-
 class Login extends Component {
-
-    componentDidMount(){
-        if(this.Auth.loggedIn()){
-            console.log(this.Auth.loggedIn())
-            this.props.history.push("/")
-        } 
+  componentDidMount() {
+    if (this.Auth.loggedIn()) {
+      console.log(this.Auth.loggedIn());
+      this.props.history.push("/");
     }
+  }
 
-    Auth = new AuthService();
+  Auth = new AuthService();
 
-    handleChange = (e) => {
-        this.setState({[e.target.name]: e.target.value})
-    }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    handleFormSubmit = (e) => {
-        e.preventDefault();
+  handleFormSubmit = e => {
+    e.preventDefault();
 
-        this.Auth.login(this.state.username, this.state.password)
-        .then(res => {
-            this.props.history.replace("/")
-        })
-        .catch(err => {
-            console.log(err)
-        });
-        
-    }
+    this.Auth.login(this.state.username, this.state.password)
+      .then(res => {
+        this.props.history.replace("/");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-
-    render() {
-        return (
-            <div className="center">
-                <div className="card">
-                    <h1>Login</h1>
-                    <form>
-                        <input
-                            className="form-item"
-                            placeholder="Username goes here..."
-                            name="username"
-                            type="text"
-                            onChange={this.handleChange}
-                        />
-                        <input
-                            className="form-item"
-                            placeholder="Password goes here..."
-                            name="password"
-                            type="password"
-                            onChange={this.handleChange}
-                        />
-                        <input
-                            className="form-submit"
-                            value="SUBMIT"
-                            type="submit"
-                        />
-                    </form>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="center">
+        <div className="card">
+          <h1>Login</h1>
+          <form>
+            <input
+              className="form-item"
+              placeholder="Username goes here..."
+              name="username"
+              type="text"
+              onChange={this.handleChange}
+            />
+            <input
+              className="form-item"
+              placeholder="Password goes here..."
+              name="password"
+              type="password"
+              onChange={this.handleChange}
+            />
+            <input className="form-submit" value="SUBMIT" type="submit" />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
