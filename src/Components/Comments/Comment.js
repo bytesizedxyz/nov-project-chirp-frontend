8d8c0dce3364c6d4ac562d15a8c55a766fd56d75
Christopher White <christopher.evan.white88@gmail.com>
2018-11-28 15:06:10 -0700

    updated commenting functionality

--- src/Components/Comments/Comment.js
+++ src/Components/Comments/Comment.js
@@ -1,58 +1,92 @@
-import React, { Component } from "react";
-import "../Post/Post.css";
+import React, { Component } from 'react';
+import '../Post/Post.css';
 
 class Comment extends Component {
   state = {
-    comments: [{ userName: "A User: ", comment: "I WIN" }],
-    commentValue: ""
+    comments: [],
+    commentValue: '',
+    commentBoxOpen: false
+  };
+  // { userName: '', comment: '' }, { userName: 'White Dads Answering The Phone: ', comment: 'Yellow' }
+
+  toggleBox = () => {
+    const { commentBoxOpen } = this.state;
+    this.setState({ commentBoxOpen: !commentBoxOpen }, () => {
+      console.log(commentBoxOpen);
+    });
   };
 
   handleChange = e => {
-    console.log("handling comment change");
+    console.log('handling comment change');
     this.setState({ commentValue: e.target.value });
   };
 
   submitComment = () => {
-    console.log("submitting comment");
-    const user = "fake user";
+    console.log('submitting comment');
+    const user = 'fake user';
     const { commentValue } = this.state;
     const newComment = { userName: user, comment: commentValue };
     this.setState({ comments: this.state.comments.concat(newComment) });
+    this.toggleBox();
+
+    // url for POST
+    // data object for POST
+    // url for GET
+    // data object received from GET
   };
 
   render() {
-    const { comments, commentValue } = this.state;
-    return (
-      <div className="comment-body">
-        <span data-testid="comments">
-          {comments.map((comment, index) => {
-            return (
-              <span key={index} className="comment-header">
+    const { comments, userName } = this.state;
+    const returnComment = () => {
+      if (comments) {
+        return comments.map(comment => {
+          return (
+            <span key={`${comment}${userName}`} data-testid="comments">
+              <span className="comment-header">
                 <img
                   className="comment-profile-image"
                   src="https://www.neweurope.eu/wp-content/uploads/2018/02/h_53880267.jpg"
                   alt="A user visual identifier."
                 />
-                <h4>{comment.userName}</h4>|<p>{comment.comment}</p>
+                <h4>{comment.userName}</h4>
+                <p>{comment.comment}</p>
               </span>
-            );
-          })}
-          <span className="reply-bar">
-            <p>Like</p>
-            <p>Hate</p>
-            <p>Reply</p>
-          </span>
-        </span>
+              <span className="reply-bar">
+                <p>Like</p>
+                <p>Hate</p>
+              </span>
+            </span>
+          );
+        });
+      }
+    };
 
-        <span className="reply-input">
-          <input
-            placeholder="New Comment"
-            value={commentValue}
-            onChange={this.handleChange}
-            type="text"
-          />
-          <p onClick={this.submitComment}>Send</p>
+    const returnCommentBox = () => {
+      const { commentValue, commentBoxOpen } = this.state;
+      if (commentBoxOpen) {
+        return (
+          <span className="reply-input">
+            <input
+              placeholder="New Comment"
+              value={commentValue}
+              onChange={this.handleChange}
+              type="text"
+            />
+            <p onClick={this.submitComment}>Send</p>
+          </span>
+        );
+      } else {
+        return <span />;
+      }
+    };
+
+    return (
+      <div className="comment-body">
+        <span className="reply-bar">
+          <p onClick={this.toggleBox}>Comment</p>
         </span>
+        <span>{returnComment()}</span>
+        <span>{returnCommentBox()}</span>
       </div>
     );
   }
--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,38 +1,42 @@
 import React, { Component } from 'react';
-import withAuth from "../../Components/withAuth";
-import { withRouter } from "react-router";
+import withAuth from '../../Components/withAuth';
+import { withRouter } from 'react-router';
 
-import Header from "../../Components/Header/Header";
-import Feed from "../../Components/Feed/Feed";
-import { ThemeContext, themes } from "../../ThemeProvider";
-
-
+import Header from '../../Components/Header/Header';
+import Feed from '../../Components/Feed/Feed';
+import { ThemeContext } from '../../ThemeProvider';
 
 class Home extends Component {
-	render() {
-		const {user, chirps, handleFilter, addPost, filter} = this.props.authProps;
-		console.log("HomeProps",this.props)
-		console.log(chirps)
-		return (
-			<ThemeContext.Consumer>
-			{({ theme }) => (
-				<div
-					className={`App
+  render() {
+    const {
+      user,
+      chirps,
+      handleFilter,
+      addPost,
+      filter
+    } = this.props.authProps;
+    console.log('HomeProps', this.props);
+    console.log(chirps);
+    return (
+      <ThemeContext.Consumer>
+        {({ theme }) => (
+          <div
+            className={`App
 					${theme.lightBlueBackground}
 					${theme.blackBackground}`}
-				>
-					<Header
-						handleFilter={handleFilter}
-						addPost={addPost}
-						user={user.attributes}
-						filter={filter}
-					/>
-					{chirps ? <Feed chirps={chirps} /> : null}
-				</div>
-			)}
-		</ThemeContext.Consumer>
-		)
-	}
+          >
+            <Header
+              handleFilter={handleFilter}
+              addPost={addPost}
+              user={user.attributes}
+              filter={filter}
+            />
+            {chirps ? <Feed chirps={chirps} /> : null}
+          </div>
+        )}
+      </ThemeContext.Consumer>
+    );
+  }
 }
 
 export default withRouter(withAuth(Home));
