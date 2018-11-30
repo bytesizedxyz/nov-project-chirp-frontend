039fc9f04d9a5f27860a29306e8a9a68bbd6733a
Kevin Griego <kev.gri32@gmail.com>
2018-11-30 14:27:18 -0700

    some shit

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Form, Card } from "semantic-ui-react";
+import { Grid, Form, Card, Container } from "semantic-ui-react";
 import "./Login.css";
 
 import AuthService from "../../Services/AuthService";
@@ -42,30 +42,35 @@ class Login extends Component {
 
   render() {
     return (
-      <Grid className="loginForm" centered columns={2} textAlign="center" verticalAlign="middle">
-        <Grid.Row centered>
-          <Card>
-            <Card.Header>Login</Card.Header>
-            <Form onSubmit={this.handleFormSubmit}>
-              <Form.Input
-                fluid
-                placeholder="Username goes here..."
-                name="username"
-                label="User Name"
-                type="text"
-                onChange={this.handleChange}
-              />
-              <Form.Input
-                placeholder="Password goes here..."
-                name="password"
-                type="password"
-                onChange={this.handleChange}
-              />
-              <Form.Button value="SUBMIT">Login</Form.Button>
-            </Form>
-          </Card>
-        </Grid.Row>
-      </Grid>
+      <Container fluid inverted className="backgroundColorBlacck">
+        <Grid className="loginForm" centered columns={2} textAlign="center" verticalAlign="middle">
+          <Grid.Row centered>
+            <Card>
+              <Card.Header style={{ margin: "20px" }}>Login</Card.Header>
+              <Form onSubmit={this.handleFormSubmit}>
+                <Form.Input
+                  fluid
+                  placeholder="Username goes here..."
+                  name="username"
+                  label="User Name"
+                  type="text"
+                  onChange={this.handleChange}
+                />
+                <Form.Input
+                  placeholder="Password goes here..."
+                  name="password"
+                  label="Password"
+                  type="password"
+                  onChange={this.handleChange}
+                />
+                <Form.Button value="SUBMIT" style={{ marginBottom: "20px" }}>
+                  Login
+                </Form.Button>
+              </Form>
+            </Card>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
