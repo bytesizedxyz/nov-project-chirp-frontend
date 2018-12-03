433bb7384184076a49cd8f278e80bbf98ba4cff0
David Kruse <thedavidkruse@gmail.com>
2018-12-03 10:42:55 -0700

    gotten chirps

--- src/App.js
+++ src/App.js
@@ -27,7 +27,11 @@ class App extends Component {
   }
 
   async componentWillMount() {
-    let chirps = await fetch("https://nov-chirp-backend.herokuapp.com/chirp");
+    let chirps = await fetch("https://nov-chirp-backend.herokuapp.com/chirp", {
+      headers: {
+        "Authorization": "Bearer " + localStorage.getItem("id_token")
+      }
+    });
     chirps = await chirps.json();
     chirps = chirps.reverse();
     console.log('chirps', chirps);
