5bee8bc5a0dce0b4d33078cb07755fea1aedf5fa
Christopher White <christopher.evan.white88@gmail.com>
2018-12-04 16:08:30 -0700

    updated overwritten code

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+// {"userId":7,"message":"Distributed solution-oriented contingency","deleted":true,"likes":16,"dislikes":33,"favorites":73,"created_at":"8/25/2001"},
 import React from 'react';
 import './Post.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,71 +14,36 @@ import Comment from '../Comments';
 import Gravatar from 'gravatar-react';
 
 export default function Post(props) {
-  const { chirp } = props;
-  const user = JSON.parse(localStorage.getItem('_user_prof')).user;
   const email = JSON.parse(localStorage.getItem('_user_prof')).email;
-  // const updateVotes = e => {
-  //   const voteName = e.target.dataset.testid;
-  //   const uuid = chirp.uuid;
-  //   console.log(voteName, uuid);
-  //   fetch(
-  //     `https://nov-chirp-backend.herokuapp.com/chirp/reaction/${voteName}/${uuid}`,
-  //     {
-  //       method: 'PUT',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         Authorization: 'Bearer ' + localStorage.getItem('id_token')
-  //       }
-  //     }
-  //   )
-  //     .then(res => res.json())
-  //     .then(res => {
-  //       console.log('I AM THE ONE AND ONLY DATAMIN', res.data);
-  //     })
-  //     .catch(err => {
-  //       console.log('error happened, printed below');
-  //       console.log(err);
-  //     });
-
-  // console.log(wat);
-  // };
-=======
-// {"userId":7,"message":"Distributed solution-oriented contingency","deleted":true,"likes":16,"dislikes":33,"favorites":73,"created_at":"8/25/2001"},
-import React from "react";
-import "./Post.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbsUp, faThumbsDown, faStar, faClock } from "@fortawesome/free-solid-svg-icons";
-import { Card } from "semantic-ui-react";
-import { ThemeContext } from "../../ThemeProvider";
-import Comment from "../Comments";
-
-export default function Post(props) {
-  const { chirp, user, updateChirp } = props;
+  const user = JSON.parse(localStorage.getItem('_user_prof')).user;
+  const { chirp, updateChirp } = props;
   const updateVotes = e => {
     const voteName = e.target.dataset.testid;
     const uuid = chirp.uuid;
-    fetch(`https://nov-chirp-backend.herokuapp.com/chirp/reaction/${voteName}/${uuid}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("id_token")
+    fetch(
+      `https://nov-chirp-backend.herokuapp.com/chirp/reaction/${voteName}/${uuid}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('id_token')
+        }
       }
-    })
+    )
       .then(res => res.json())
       .then(res => {
         const objKeys = Object.keys(res.data);
         updateChirp({ [objKeys[0]]: res.data[objKeys[0]] }, uuid);
       })
       .catch(err => {
-        console.log("error happened, printed below");
+        console.log('error happened, printed below');
         console.log(err);
       });
   };
->>>>>>> 49be8415bbe8fa2bb0e918072d63afa9a63872b4
   return (
     <ThemeContext.Consumer>
       {({ theme, toggleTheme }) => (
-        <Card color={theme.brownBackground ? "red" : "white"}>
+        <Card color={theme.brownBackground ? 'red' : 'white'}>
           <Card.Content>
             <Card.Header>
               <span className="profile-info">
@@ -111,17 +76,26 @@ export default function Post(props) {
               <span className="stats">
                 <span className="votes">
                   <span data-testid="like" onClick={updateVotes}>
-                    {chirp.likes} <FontAwesomeIcon color={theme.iconColor} icon={faThumbsUp} />
+                    {chirp.likes}{' '}
+                    <FontAwesomeIcon
+                      color={theme.iconColor}
+                      icon={faThumbsUp}
+                    />
                   </span>
                   <span data-testid="hate" onClick={updateVotes}>
-                    {chirp.hates} <FontAwesomeIcon color={theme.iconColor} icon={faThumbsDown} />
+                    {chirp.hates}{' '}
+                    <FontAwesomeIcon
+                      color={theme.iconColor}
+                      icon={faThumbsDown}
+                    />
                   </span>
                   <span data-testid="favorite" onClick={updateVotes}>
-                    {chirp.favorites} <FontAwesomeIcon color={theme.iconColor} icon={faStar} />
+                    {chirp.favorites}{' '}
+                    <FontAwesomeIcon color={theme.iconColor} icon={faStar} />
                   </span>
                 </span>
                 <span className="date">
-                  {new Date(chirp.created_at).toLocaleDateString() + " "}
+                  {new Date(chirp.created_at).toLocaleDateString() + ' '}
                   <FontAwesomeIcon color={theme.iconColor} icon={faClock} />
                 </span>
               </span>
